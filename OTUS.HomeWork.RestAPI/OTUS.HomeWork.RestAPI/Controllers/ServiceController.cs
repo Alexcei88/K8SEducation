7019c56e446e3036c1ea5e130181ebf5e4844e56
Alexcei Shmakov <alexcei.shmakov@gmail.com>
2021-01-27 22:58:48 +0300

    Deploy a simple service in the K8S

---
+++ OTUS.HomeWork.RestAPI/OTUS.HomeWork.RestAPI/Controllers/ServiceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OTUS.HomeWork.RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+		/// <summary>
+		/// Проверка доступности сервисов
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		[Route("/health")]
+		public ActionResult HealthCheck()
+		{
+			return Ok();
+		}
+	}
+}
